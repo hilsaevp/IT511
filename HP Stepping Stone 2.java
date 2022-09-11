533ee56e4a96bef4ef28e3594d5df25588ba799d
hep-15 <92876767+hep-15@users.noreply.github.com>
2022-09-11 19:01:37 -0400

    Update HP Stepping Stone 2.java

--- HP Stepping Stone 2.java
+++ HP Stepping Stone 2.java
@@ -1,8 +1,3 @@
-/**
- *
- * @author Hilsa Parinding
- */
-
 package SteppingStones;
 
 import java.util.Scanner;
@@ -11,6 +6,7 @@ public class SteppingStone2_IngredientCalculator {
 
     /**
      * @param args the command line arguments
+     * @author Hilsa Parinding
      */
     public static void main(String[] args) {
        
